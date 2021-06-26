c1328ea47a4027ac02d26b82b6143030676790bc
Vaibhav Chauhan <cvaibhav0320@gmail.com>
2021-06-26 12:19:57 +0530

    INput form

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true, url: "*youtube.com*" };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  console.log(tab)
+  return tab;
+}
--- popup.js
+++ popup.js
@@ -1,23 +1,10 @@
-let changeColor = document.getElementById("changeColor");
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+function click_handler() {
+  console.log("here");
+  var url = document.getElementById('url').value;
+  console.log(url);
+ 
+}
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("CLick");
+  document.getElementById("submit").onclick = click_handler;
 });
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
-  });
-  
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
