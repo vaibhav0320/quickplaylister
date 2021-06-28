fda62399d19b0181af71a3b1be1dae5a9df379a1
Vaibhav Chauhan <cvaibhav0320@gmail.com>
2021-06-28 23:53:02 +0530

    form; message passing; init;

--- background.js
+++ background.js
@@ -1,6 +1,31 @@
-async function getCurrentTab() {
-  let queryOptions = { active: true, currentWindow: true, url: "*youtube.com*" };
+var queue = new Array();
+var main_tab;
+async function getCurrentTab(uurl) {
+  let queryOptions = {active: true, currentWindow: true, url: uurl};
   let [tab] = await chrome.tabs.query(queryOptions);
-  console.log(tab)
   return tab;
+}
+
+function init(url){
+  queue.push(url);
+  main_tab = getCurrentTab(url);
+  console.log(main_tab);
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    //console.log("back : from the extension");
+    console.log(request);
+    if(queue.length == 0){
+      init(request.url);
+    }
+  
+  console.log(queue);
+  console.log(main_tab);
+  sendResponse({send: "ack"});
+}
+);
+
+async function store_info() {
+  
 }
--- popup.js
+++ popup.js
@@ -1,10 +1,36 @@
+var queue = new Array();
+
+
+function add_to_queue(url){
+
+
+  chrome.runtime.sendMessage({url: url}, function(response) {
+      if(response.send != "ack"){
+        console.log('internal error !!');
+        return -1;
+      }
+      console.log("ack");
+  });
+
+
+  queue.push(url);
+  //console.log(queue);
+
+}
+
 function click_handler() {
-  console.log("here");
   var url = document.getElementById('url').value;
-  console.log(url);
+  var res = url;
+
+/* String matching function needs to be added */
+ // var res = url.match(/aa/i);  
+  if(res == null){
+    return;
+  }
+  console.log(res);
+  add_to_queue(res);
  
 }
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("CLick");
   document.getElementById("submit").onclick = click_handler;
 });
